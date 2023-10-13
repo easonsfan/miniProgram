921c01ebe433151192929ee489fc22295524804b
easonsfan <1275422527@qq.com>
2023-10-13 17:28:00 +0800

    歌单详情介绍

--- pages/playlist-detail/playlist-detail.vue
+++ pages/playlist-detail/playlist-detail.vue
@@ -1,10 +1,36 @@
 <template>
 	<view>
-		<view class="desc">
-			1
+		<view class="playlist-info">
+			<image class="cover" :src="playlistInfo.cover" mode="aspectFill"></image>
+			<view class="desc">
+				<image class="cover" :src="playlistInfo.cover" ></image>
+				<view class="info">
+					<text class="name">{{playlistInfo.name}}</text>
+					<view class="creator">
+						<image class="avatar" :src="playlistInfo.creator.avatar"></image>
+						<text class="nickname">{{playlistInfo.creator.nickname}}</text>
+					</view>
+				</view>
+			</view>
+			<view class="descText">
+				<text class="text">简介：{{playlistInfo.description?.replace(/[\n]/g,' ')}}</text>
+				<uv-icon name="arrow-right" size="28rpx"></uv-icon>
+			</view>
+			<view class="function">
+				<view class="like">
+					<uv-icon name="heart" size="40rpx"></uv-icon>
+					<text>{{playlistInfo.subscribedCount}}</text>
+				</view>
+				<view class="share">
+					<uv-icon name="share-square" size="40rpx"></uv-icon>
+					<text>分享</text>
+				</view>
+			</view>
 		</view>
 		<view class="song-list">
-			2
+			<view class="wrapper">
+				<text class="order"></text>
+			</view>
 		</view>
 	</view>
 </template>
@@ -12,15 +38,101 @@
 <script setup>
 	import {onLoad} from '@dcloudio/uni-app'
 	import { ref } from "vue";
+	import { storeToRefs } from 'pinia'
 	import {usePlaylistDetailStore} from '@/store/playlist-detail/index.js'
 	const playlistDetailStore = usePlaylistDetailStore()
+	const {playlistInfo,songList } = storeToRefs(playlistDetailStore)
 	onLoad((options)=>{
+		// playlistDetailStore.getPlaylistDetail(7688058643)
 		playlistDetailStore.getPlaylistDetail(options.id)
 	})
 </script>
 
 <style lang="scss">
-.desc{
-	
+.playlist-info{
+	height: 440rpx;
+	overflow: hidden;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	color: #fff;
+	>.cover{
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		filter: blur(5px);
+		z-index: -1;
+	}
+	.desc{
+		display: flex;
+		margin-top: 60rpx;
+		padding: 0 40rpx;
+		.cover{
+			width: 240rpx;
+			height: 240rpx;
+			border-radius: 10rpx;
+		}
+		.info{
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			flex: 1;
+			margin-left: 20rpx;
+			.name{
+				display: inline-block;
+				width: 100%;
+			}
+			.creator{
+				display: flex;
+				align-items: center;
+				.avatar{
+					width: 50rpx;
+					height: 50rpx;
+					border-radius: 50%;
+					margin-right: 10rpx;
+				}
+				.nickname{
+					font-size: 24rpx;
+				}
+			}
+		}
+		.content{
+			font-size: 24rpx;
+		}
+	}
+	.descText{
+		margin-top: 20rpx;
+		font-size: 24rpx;
+		padding: 0 40rpx;
+		.text{
+			display:inline-block;
+			max-width: 90%;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+		.uv-icon{
+			display: inline-block !important;
+			overflow: hidden;
+			.uvicon-arrow-right{
+				color: #fff !important;
+			}
+		}
+	}
+	.function{
+		display: flex;
+		justify-content: space-around;
+		font-size: 28rpx;
+		margin-top: 20rpx;
+		.like,.share{
+			display: flex;
+		}
+		.uv-icon{
+			margin-right: 10rpx;
+			.uv-icon__icon{
+				color: #fff !important;
+			}
+		}
+	}
 }
 </style>
--- store/playlist-detail/index.js
+++ store/playlist-detail/index.js
@@ -1,14 +1,43 @@
 import {defineStore} from 'pinia'
 import { getPlaylistDetail } from '@/services/playlist-detail/playlist-detail'
+import { markRaw } from 'vue'
 export const usePlaylistDetailStore = defineStore('playlistDetail',{
 	state:()=>{
 		return{
-			
+			playlistInfo:{
+				cover:'',
+				name:null,
+				creator:{},
+				description:null,
+				subscribedCount:0
+			},
+			songList:[],
 		}
 	},
 	actions:{
 		async getPlaylistDetail(id){
 			const res = await getPlaylistDetail({id})
+			if(res.code == 200){
+				this.playlistInfo['cover'] = res.playlist.backgroundCoverUrl||res.playlist.coverImgUrl
+				this.playlistInfo['description'] = res.playlist.description
+				console.log(this.playlistInfo['description']);
+				this.playlistInfo['name'] = res.playlist.name
+				this.playlistInfo['subscribedCount'] = res.playlist.subscribedCount
+				this.playlistInfo['creator'] = {
+					avatar: res.playlist.creator.avatarUrl,
+					nickname: res.playlist.creator.nickname,
+				}
+				const list = res.playlist.tracks.map(item=>{
+					return {
+						id:item.id,
+						name:item.name,
+						singer: item.ar.map(ele=>{
+							return ele.name
+						}).join(' & ')
+					}
+				})
+				this.songList = markRaw(list)
+			}
 		}
 	}
 })
